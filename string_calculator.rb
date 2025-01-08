c25bc9abb3bf3139497f3f2eff18672a36b6a1cc
anandsinghs257 <anandsinghs257@gmail.com>
2025-01-09 01:20:20 +0530

    handle two numbers seprated by comma

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,5 @@
 def add(numbers)
   return 0 if numbers.empty?
 
-  numbers.to_i
+  numbers.split(',').map(&:to_i).sum
 end
--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -10,5 +10,10 @@ RSpec.describe 'String Calculator' do
       expect(add("1")).to eq(1)
       expect(add("7")).to eq(7)
     end
+
+    it 'returns the sum of two numbers separated by a comma' do
+      expect(add("1,2")).to eq(3)
+      expect(add("3,4")).to eq(7)
+    end
   end  
 end    
